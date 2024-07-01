80c80f29a6672de6f15ea4ce511b18e10a28e1bd
olimpiaasm <126819162+olimpiaasm@users.noreply.github.com>
2024-07-01 22:04:19 +0200

    Aplicación terminada y funcional definitiva y con retoques terminados

--- app/src/main/java/com/example/signapp/NotificacionActivity.java
+++ app/src/main/java/com/example/signapp/NotificacionActivity.java
@@ -1,16 +1,20 @@
 package com.example.signapp;
 
+import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.List;
 
@@ -19,6 +23,7 @@ public class NotificacionActivity extends AppCompatActivity {
     private static final String CHANNEL_ID = "signapp_notifications";
     private static final String CHANNEL_NAME = "SignApp Notifications";
     private static final String CHANNEL_DESC = "Notifications for SignApp events";
+    private static final int REQUEST_CODE_POST_NOTIFICATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,12 @@ public class NotificacionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_notificaciones);
 
         createNotificationChannel(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, REQUEST_CODE_POST_NOTIFICATION);
+            }
+        }
+
 
         ListView listView = findViewById(R.id.listViewNotifications);
         List<String> notifications = AlmacenamientoNotificacion.getNotifications(this);
